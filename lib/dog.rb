f213c134ceb05c40a95fe4b6290bb2ce5086e89c
Kayla Cross <kac288@georgetown.edu>
2018-06-27 00:25:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,14 +37,14 @@ class Dog
     Dog.new(id: row[0], name: row[1], breed: row[2])
   end
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT id, name, breed FROM dogs
-    WHERE name = ?
-    SQL
-
-    row = DB[:conn].execute(sql, name)
-  end
+  # def self.find_by_name(name)
+  #   sql = <<-SQL
+  #   SELECT id, name, breed FROM dogs
+  #   WHERE name = ?
+  #   SQL
+  #
+  #   row = DB[:conn].execute(sql, name)
+  # end
 
   def update
   end
@@ -76,10 +76,17 @@ class Dog
   end
 
   def self.find_or_create_by(name)
-    if self.find_by_name(name).empty?
+    sql = <<-SQL
+    SELECT id, name, breed FROM dogs
+    WHERE name = ?
+    SQL
+
+    row = DB[:conn].execute(sql, name)
+
+    if self.row.empty?
       self.create(name)
     else
-      self.find_by_name(name)
+      self
     end
   end
 
